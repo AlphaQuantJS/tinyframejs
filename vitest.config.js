79f3605e067a26bcc9c294cfe8a03da21dadeeb6
Alex K <alex.kurz@protonmai.com>
2025-04-25 02:29:35 +0200

    ci(test): allow missing coverage reports without failing CI

--- vitest.config.js
+++ vitest.config.js
@@ -1,8 +1,10 @@
 export default {
   test: {
+    passWithNoTests: true,
     coverage: {
       provider: 'v8',
       reporter: ['text', 'lcov'],
+      exclude: ['*.config.js'],
     },
   },
 };

c38c24871d9167b066fd3d248914cc6c9cd5a4a5
Alex K <alex.kurz@protonmai.com>
2025-04-25 02:03:02 +0200

    test: add initial sanity test to pass CI

---
+++ test/sanity.test.js
@@ -0,0 +1,8 @@
+// test/sanity.test.js
+import { describe, it, expect } from 'vitest';
+
+describe('CI Sanity Check', () => {
+  it('should always pass', () => {
+    expect(true).toBe(true);
+  });
+});

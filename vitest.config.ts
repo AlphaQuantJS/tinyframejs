c7b926f50145554176701d6a4258a230845b04b8
Alex K <alex.kurz@protonmai.com>
2025-04-25 02:11:06 +0200

    style: format vitest.config.ts to match Prettier rules

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,10 @@
 // vitest.config.ts
 export default {
-    test: {
-      include: ['test/**/*.test.js'],
-      coverage: {
-        provider: 'c8',
-        reporter: ['text', 'lcov']
-      },
+  test: {
+    include: ['test/**/*.test.js'],
+    coverage: {
+      provider: 'c8',
+      reporter: ['text', 'lcov'],
     },
-  };
-  
-
-  
+  },
+};
